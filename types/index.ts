9b4a69ad6609729270be39749e866d87c1010aed
devsafix <devsafix@gmail.com>
2025-11-16 09:18:07 +0600

    Add: Types for user, post and pagination

---
+++ types/index.ts
@@ -0,0 +1,32 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  picture: string | null;
+  status: string;
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  content: string;
+  thumbnail: string | null;
+  isFeatured: boolean;
+  tags: string[];
+  views: number;
+  authorId: number;
+  createdAt: string; // Dates will come as strings
+  updatedAt: string;
+  author: User;
+}
+
+// Type for the paginated response from our backend
+export interface PostsResponse {
+  data: Post[];
+  pagination: {
+    page: number;
+    limit: number;
+    totalData: number;
+    totalPages: number;
+  };
+}
